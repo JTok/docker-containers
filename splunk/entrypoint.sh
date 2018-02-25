8791dad64717e024113121eba6ad555a63926c6a
JTok <1851873+JTok@users.noreply.github.com>
2018-02-25 16:21:17 -0600

    add optimistic file locking

--- splunk/entrypoint.sh
+++ splunk/entrypoint.sh
@@ -23,6 +23,11 @@ elif [ "$1" = 'start-service' ]; then
   # If these files are different override etc folder (possible that this is upgrade or first start cases)
   # Also override ownership of these files to splunk:splunk
   if ! $(cmp --silent /var/opt/splunk/etc/splunk.version ${SPLUNK_HOME}/etc/splunk.version); then
+    # Set optimistic file locking
+    if ! grep -i "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" /var/opt/splunk/etc/splunk-launch.conf
+    then
+      printf "\nOPTIMISTIC_ABOUT_FILE_LOCKING = 1\n" >> /var/opt/splunk/etc/splunk-launch.conf
+    fi
     cp -fR /var/opt/splunk/etc ${SPLUNK_HOME}
     chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/etc
     chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/var
@@ -147,7 +152,7 @@ EOF
       fi
     done
   fi
-  
+
   sudo -HEu ${SPLUNK_USER} tail -n 0 -f ${SPLUNK_HOME}/var/log/splunk/splunkd_stderr.log &
   wait
 elif [ "$1" = 'splunk-bash' ]; then
