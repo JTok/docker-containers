9d7cb14b8a416f67753b28a4e876fed0a550a5e0
JTok <1851873+JTok@users.noreply.github.com>
2018-02-25 19:56:45 -0600

    Update entrypoint.sh

--- splunk/entrypoint.sh
+++ splunk/entrypoint.sh
@@ -72,7 +72,16 @@ EOF
       fi
     done
   fi
-
+  
+  # Set optimistic file locking in main location
+  if ! grep -iq "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" ${SPLUNK_HOME}/etc/splunk-launch.conf; then
+    printf "\nOPTIMISTIC_ABOUT_FILE_LOCKING = 1\n" >> ${SPLUNK_HOME}/etc/splunk-launch.conf
+  fi
+  # Set optimistic file locking in backup location
+  if ! grep -iq "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" ${SPLUNK_BACKUP_DEFAULT_ETC}/etc/splunk-launch.conf; then
+    printf "\nOPTIMISTIC_ABOUT_FILE_LOCKING = 1\n" >> ${SPLUNK_BACKUP_DEFAULT_ETC}/etc/splunk-launch.conf
+  fi
+  
   sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk start ${SPLUNK_START_ARGS}
   trap "sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk stop" SIGINT SIGTERM EXIT
 
